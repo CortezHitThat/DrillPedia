a4e30f906082ac18241c91c002d8732ce491f1f7
CortezHitThat <escobarcortez999@gmail.com>
2025-04-29 21:42:21 +0700

    Add GoatCounter tracking

--- deads/deads.js
+++ deads/deads.js
@@ -10,5 +10,56 @@ const deads = [
     ],
     snippet: "Dummout Hound adalah kawan baik...",
     full: "Dummout Hound AKA Dummy(G$ideDrilly/🐺🅱️🎩 )kawan baik pencipta aliansi drilly,yaitu spazz drilly. setelah dummy meninggal,Spazz Drilly (G$ideDrilly/🐺🅱️🎩)Membuat aliansi bernama Drilly Gang Atau Devoted Resilient Intelligent Living Lavish Young Gang Di tahun 2018. Dummy adalah Seorang Gside sebelum drilly tercipta,setnya adalah bloodhound brims,dia meninggal karna ditusuk di dada nya hingga tewas di perkelahian melawan seorang crip bernama roshawn pine,seorang Rollin crip yang berada di dekalb. Yang sekarang dirinya dihukum di penjara selama 14 tahun karna pembunuhan dummy/dummout hound."
+  },
+  
+  {
+    id: "woo-lotti",
+    title: "Woo Lotti",
+    date: "2025-04-29",
+    images: [
+      "images/lotti.jpg",
+      "images/lotti2.jpg",
+      "images/lotti3.jpg"
+    ],
+    snippet: "Woo Lotti sedang spinning blok OY...",
+    full: "Kisah Kematian Woo Lotti 🕊️Woo Lotti (143Rd/💚🐰) lagi memutari blok OY dengan Sha gz (ECG/💚🐰), Chubz (Pologrounds/💚🐰), Gee Gotti (Pologrounds/💚🐰), Savo (Pologrounds/💚🐰), Quando (Pologrounds/💚🐰) saat sedang melewati jalan W 154th St, St Nicholas Avenue yang dimana itu adalah wilayah OY, OY Rundown (🍇♿), Dolo Kev (♨️⏰🅱️),Sugarhill Smokey (🦍🅱️), AT Balla  (〽️🐝), Blockwork (⛪) melihat mereka lalu mereka (YGz) berhamburan menyelamatkan diri dan sha gz mencoba untuk menembak tetapi pistolnya macet lalu ia lari meninggalkan woo lotti dibelakang, woo lotti masih berlari dan terjatuh lalu ia ditusuk hingga m4t! oleh OY Rundown (🍇♿), Dolo Kev (♨️⏰🅱️),Sugarhill Smokey (🦍🅱️), AT Balla  (〽️🐝), dan Blockwork (⛪) merekam kejadian itu."
+  },
+  
+  {
+    id: "rah-gz",
+    title: "Rah Gz",
+    date: "2025-04-29",
+    images: [
+      "images/rah.jpg",
+      "images/rah2.jpg",
+      "images/rah3.jpg"
+    ],
+    snippet: "Rah Gz serang menaiki taksi...",
+    full: "Pada 11 Juli 2021, Ramon ''Rah Gz'' Gil-Medrano (🕊️) (💚🐰/80h) yang berusia 16 tahun sedang menaiki Uber saat berbicara di telepon dengan Nesty Floxks(🐺🎩🅱️), ke studio karena ia ingin menghina Jaryan ''JayRipK'' Elliot (🕊️) (✌🏾🎱🎯/3rdSide) yang meninggal pada hari yang sama. 2 Tersangka yang kemudian diidentifikasi sebagai Almon (〽🐝/40Projects) & NoLuv Munna (✌🏾🎱🎯/3rdSide) sedang mengendarai skuter dan mulai menembak, mengenai kepala ''Rah Gz''. Kemudian dikatakan dan dikonfirmasi bahwa Rico Balla (〽🐝/Mantan 8oh) memberi tahu Almon (〽🐝/40Projects) & NoLuv Munna (✌🏾🎱🎯/3rdSide) lokasi di mana Rah Gz berada. Pembunuh: Almon (〽🐝/40z) & NoLuv Munna (✌🏾🎱🎯/3rdSide)"
+  },
+
+  {
+    id: "ue",
+    title: "Ue",
+    date: "2025-04-29",
+    images: [
+      "images/ue.jpg",
+      "images/cream.jpg"
+    ],
+    snippet: "Ue dari HBTL adalah seorang...",
+    full: "UE(HBTL/♨️⏱️🎩)Adalah seorang Prime time brims yang mati ditembak dikepala oleh Cream Hound(RPT/🅱️🐺🎩)  Yang nanti akan menjadi alasan kenapa Cream Hound(RPT/🅱️🐺🎩) dipenjara 28 Tahun."
+  },
+  
+  {
+    id: "noah-balla",
+    title: "Noah Balla",
+    date: "2025-04-29",
+    images: [
+      "images/noah.jpg",
+      "images/noah2.jpg",
+      "images/noah3.jpg"
+    ],
+    snippet: "Noah sedang mengobrol dengan...",
+    full: "Moises ''Noah Balla'' Lora (⭕💤,〽️🐝/COE ) sedang mengobrol dengan 2 perempuan dan satu pria. setelah berbincang-bincang, satu perempuan ini kembali pulang ke rumahnya  (Melrose Houses) setelah mendapatkan panggilan telpon dari ibu nya. lalu anggota YGz (💚🐰) melihat Noah sendirian lalu mengejarnya. sayangnya Noah terjatuh lalu anggota YGz menendang Noah. Salah satu member YGZ, yaitu Delly (143rd/💚🐰) ia menginjak kepala Noah dengan brutal lalu Noah pun mengalami cedera di otak dan kepalanya, semetara itu member YGZ yang lain menendangi leher noah. Delly dipenjara 41 tahun"
   }
 ];
