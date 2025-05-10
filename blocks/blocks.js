29980918dc32c344034b76e887d48ad982b14219
CortezHitThat <escobarcortez999@gmail.com>
2025-05-10 18:38:57 +0700

    Add GoatCounter tracking

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -1,13 +1,40 @@
 const blocks = [
   {
-    id: "183rd",
-    title: "183rd Street",
-    date: "18 April 2025",
-    image: "images/183rd.jpg",
-    snippet: "183rd Street merupakan salah satu area panas di Bronx...",
-    full: `183rd Street merupakan area tempat banyak geng beroperasi,
-dan menjadi lokasi banyak insiden kekerasan terkait NYC drill scene.
-Beberapa set yang memiliki pengaruh di area ini adalah OGz dan OY.`
+    id: "9raq",
+    title: "9Raq",
+    date: "10 May 2025",
+    image: [
+      "images/raq.jpg",
+      "images/raq2.jpg",
+      "images/raq3.jpg"
+      ],
+    snippet: "9Raq...",
+    full: [
+      `9raq alias DrizzyBDrive alias NuskiGang berlokasi di 169th Street, Morris
+      Ave, College Ave & 167th Street.`,
+      
+`Mereka terdiri dari berbagai set, yaitu:
+Primetime Brims (♨️⏰🎩)
+1090Gzz (🏁)
+(BuggatiBoyz) Gorilla Stone Bloods (🦍🅱️)
+280 Gangster Crips (✌🏼🎱🎯)
+
+𝗔𝗹𝗹𝗶𝗮𝗻𝗰𝗲𝘀 & 𝗖𝗹𝗶𝗾𝘂𝗲𝘀:
+TopTweaks (👺/👹) (Alliance)
+THF (🛝) (Clique)
+
+𝗟𝗶𝗻𝗸𝗲𝗱 𝗔𝗹𝗹𝗶𝗲𝘀:
+280Blocc/JoppyBlock (4️⃣3️⃣♿,✌🏿🎱🎯,⭕💜), OBW/OriginalBreadWinnerz (⭕💜,✌🏿🎱🎯,🚼♿,⛪,🏁), OT3/3rdside (✌🏿🎱🎯,⛪,〽️🐝,🏁,🎱Ⓜ♿,8️⃣0️⃣♿,🅱️🐺🎩), McKinley/BenjiBlock (🦍🅱️,〽️🐝,⛪️,♨️🎩🅱️), Sev0/JetGang (🦍🅱️,🅱️🐺🎩), 700/TrapStarz (🏴‍☠️💚🍍,🏁,🎱Ⓜ♿,🦍🅱), 8Blocc (〽️🐝,✌🏿🎱🎯), OnlyThe6ix/7ev6ix (〽️🐝,🅱️🐺🎩,💚🍍) and lot more..
+
+𝗢𝗽𝗽𝘀:
+6Wild (〽️🐝), 1300/KeyzDrive(🐺🎩🅱️,⛪,🚼♿), GunHill/GHG (🐺🎩🅱️,⛪,♨️⏰🎩), OYG/SHGTM (⭕✌,🍇♿, 〽️🐝,🦍🅱,6️⃣2️⃣♿,6️⃣0️⃣♿,☘️♿,🐺🎩🅱️,🍍💚), 065Demons/ODS/5th/165DOA (〽️🐝,⛪,🤙🏾🧱♿,8️⃣0️⃣♿,🎱Ⓜ️♿), CourtlandtOvaEverything/OGz (⭕️,🏁,〽️🐝,🅱️🐺🎩,🦍🅱️,♨️⏰🎩,⛪,6️⃣0️⃣♿,💚🍍), HBTL/FlyBridge (〽️🐝,🅱️🐺🎩,♨️⏰🎩, 6️⃣0️⃣♿,⛪,🏁) and lot more..
+
+𝗕𝗼𝗱𝗶𝗲𝘀:
+Gee6ix (🕊️) (〽️🐝/6wild)
+
+𝗡𝗼𝘁𝗮𝗯𝗹𝗲 𝗠𝗲𝗺𝗯𝗲𝗿𝘀:
+AJ Brim (🕊️) (♨️⏰🎩,🏁), Ljay Gzz (🏁,♨️⏰🎩/700), Drizzy B (🕊️) (♨️⏰🎩), KdotKeepKlickin (🏁), Jiggy Blu (✌🏿🎱🎯/3rdside) Jah Bklu (✌🏿🎱🎯), JJ (🕊️) (🎱Ⓜ️♿), LeftyGz (🏁), 4tohisjaw80 (✌🏿🎱🎯), KK.Spinnin (🏁,🦍🅱️/700), JaDaGatti (🦍🅱️), Bubba 2Tact (🏁), Hoy Honcho (🦍🅱️), Dee Staxkz (♨️⏰🎩), D-Lo (🕊️) (🎱Ⓜ️♿), FazoMG (♨️⏰🎩), Draco (♨️⏰🎩), Bolow B (🦍🅱️,🏁), Nuski/Nunu Gzz (🕊️) (🏁,♨️⏰🎩), Tae Mufasa (🕊️) (🦍🅱️,🏁) and lot more.`
+      ],
   },
 
   {
@@ -16,8 +43,7 @@ Beberapa set yang memiliki pengaruh di area ini adalah OGz dan OY.`
     date: "19 April 2025",
     image: "images/webster.jpg",
     snippet: "Webster Avenue terkenal sebagai markas utama OY...",
-    full: `Webster Avenue adalah salah satu lokasi yang paling dikenal dalam konflik antara OY dan OGz.
-Tempat ini sering disebut dalam lagu-lagu drill dan video musik sebagai representasi kekuatan OY.`
+    full: `Webster Avenue adalah salah satu lokasi yang paling dikenal dalam konflik antara OY dan OGz. Tempat ini sering disebut dalam lagu-lagu drill dan video musik sebagai representasi kekuatan OY.`
   },
 
   // Tambah data blocks lain di sini
