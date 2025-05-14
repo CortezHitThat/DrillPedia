35947511b5aa1401dd50aed9512e1f4c092dbd98
CortezHitThat <escobarcortez999@gmail.com>
2025-05-15 00:01:17 +0700

    Add GoatCounter tracking

---
+++ js/tokens.js
@@ -0,0 +1,11 @@
+// tokens.js
+const validTokens = [
+  {
+    token: "fjq4938jfskf93jf02j3f2f3f203f", 
+    expiresAt: "2025-05-14T16:10:00.000Z"  // Waktu UTC +0
+  },
+  {
+    token: "djq29fj29fj29fj29fj29fj29fj29", 
+    expiresAt: "2025-05-14T16:11:00.000Z"
+  }
+];
